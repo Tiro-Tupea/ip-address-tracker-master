d5b3c1e5e47eb4603880a9179376c13f5eae64e2
Tiro-Tupea <69351663+Tiro-Tupea@users.noreply.github.com>
2023-01-08 16:46:32 +0100

    fixed fetch api still work to do

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     
-    const protocol    = window.location.protocol === 'https' ? 'ws://' : 'wss=//';
-    const adress      = protocol + window.location.host + window.location.pathname + '/ws';
+    const apiKey = "at_VRIs0F5PG0aWF3n5Dt7P2DIuPwWfo";
+    const geo_loc_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`
 
     const ipResult    = document.querySelector('.ip');
     const locResult   = document.querySelector('.location');
@@ -9,21 +9,17 @@ window.onload = () => {
     const ispResult   = document.querySelector('.isp');
     const srchBtn     = document.querySelector('.search-btn');
     const srchBar     = document.querySelector('input[type=text]');
-
-    srchBar.addEventListener('keypress', (e)=>{
-        let userVal = srchBar.value;
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            getIpInfo();
-            srchBar.value = '';
-        }
-    });
+    const markerIcon = L.icon({
+        iconUrl: './assets/images/icon-location.svg',
+        iconSize: [30, 40],
+      });
+      
      
 
     //initialise the map
-    var map = L.map('map').setView([0, 0], 3);//set the location map
 
-    var marker = L.marker([0,0]).addTo(map);// set the marker on th map
+    let marker;
+    var map = L.map('map').setView([0, 0], 3);//set the location map
     var popup = L.popup();//set popup by clicking on the map
     
     //display the map on the page 
@@ -41,29 +37,67 @@ window.onload = () => {
     }
 
     map.on('click', onMapClick);
-    getIpInfo();
+    
 
     
-    function getIpInfo () {
-        fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_VRIs0F5PG0aWF3n5Dt7P2DIuPwWfo&ipAddress=8.8.8.8').then(reponse => {
-            return reponse.json();
-        }).then(ipData => {
+    const get_ip_info = async (ipToLocate =  '') => {
+        try {
 
+        const res = await fetch(`${geo_loc_URL}&ipAdress=${ipToLocate}`);
+        const ipData = await res.json();
+        updateMap(ipData);
+        }
+        catch{
+            console.log(error);
+            alert('Enable to find given domain or ip');
+        }
+
+        
+    };
+
+    const get_domain_info = async (domainToLocate =  '') => {
+        try {
+
+        const res = await fetch(`${geo_loc_URL}&ipAdress=${domainToLocate}`);
+        const ipData = await res.json();
+        updateMap(ipData);
+        }
+        catch{
+            console.log(error);
+            alert('Enable to find given domain or ip');
+        }
+
+        
+    };
+
+
+
+    const updateMap = (ipData) => {
+
+        map.setView([ipData.location.lat, ipData.location.lng]);
+
+        if(!marker){
+            marker = L.marker([ipData.location.lat, ipData.location.lng],{icon: markerIcon}).addTo(map)  ;
+        }else {
+            marker.setLatLng([ipData.location.lat, ipData.location.lng]);
+        }
+            
         ipResult.innerHTML    = `${ipData.ip}`;
         const ipLocation      = ipData.location;
         locResult.innerHTML   = `${ipLocation.region} <br> ${ipLocation.city}` +' '+ `${ipLocation.postalCode}`;
         timerResult.innerHTML = `UTC${ipLocation.timezone}`;
-        ispResult.innerHTML   = `${ipData.isp}`;
+        ispResult.innerHTML   = `${ipData.isp}`;   
+    };
 
-        marker.setLatLng([ipData.location.lat, ipData.location.lng]);
-        map.setView([ipData.location.lat, ipData.location.lng]);
+    srchBar.addEventListener('keypress', (e)=>{
+        let userVal = srchBar.value;
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            get_ip_info();
+            get_domain_info();
+            srchBar.value = '';
+        }
+    });
 
-        }).catch(error => {
-            console.log(error);
-        })  
-
-        
-    }
-
-    
+    get_ip_info();
 }
